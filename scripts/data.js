7afa7d539a7bafb2005a21a721925b0935e10df0
xser56 <gettosheep56@gmail.com>
2024-12-09 16:41:26 -0800

    set up funciton

---
+++ scripts/data.js
@@ -0,0 +1,67 @@
+// Current weather tab
+let currentTemp = document.getElementById("currentTemp");
+let highTemp = document.getElementById("highTemp");
+let city = document.getElementById("city");
+let weatherIcon = document.getElementById("weatherIcon");
+
+// Recommend Tab
+let recommend = document.getElementById("recommend");
+
+// Favorite List 
+let top5List = document.getElementById("top5List");
+let top5 = [];
+
+// Weather Tabs
+let day1 = document.getElementById("day1");
+let day2 = document.getElementById("day2");
+let day3 = document.getElementById("day3");
+let day4 = document.getElementById("day4");
+
+// Test Button
+let testButton = document.getElementById("testButton");
+
+async function getAPI()
+{
+    const response = await fetch("http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=912423fbae3a70a3af91a65f583a662f")    
+    const data = await response.json();
+    console.log(data.list);
+    return data;
+}
+getAPI();
+
+testButton.addEventListener("click", async function()
+{
+   let data = await getAPI();
+   console.log("Current Day")
+   console.log("The current temperature is " + data.list[0].main.temp);
+   console.log(`Weather Icon: ${data.list[0].weather[0].description}`);
+   console.log("The min temperature is " + data.list[0].main.temp_min);
+   console.log("The max temperature is " + data.list[0].main.temp_max);
+   currentTemp.innerText = data.list[0].main.temp;
+
+   console.log("Tuesday")
+   console.log("The current temperature is " + data.list[8].main.temp);
+   console.log(`Weather Icon: ${data.list[8].weather[0].description}`);
+   console.log("The min temperature is " + data.list[8].main.temp_min);
+   console.log("The max temperature is " + data.list[8].main.temp_max);
+
+   console.log("Wednsday")
+   console.log("The current temperature is " + data.list[16].main.temp);
+   console.log(`Weather Icon: ${data.list[16].weather[0].description}`);
+   console.log("The min temperature is " + data.list[16].main.temp_min);
+   console.log("The max temperature is " + data.list[16].main.temp_max);
+
+   console.log("Thursday")
+   console.log("The current temperature is " + data.list[24].main.temp);
+   console.log(`Weather Icon: ${data.list[24].weather[0].description}`);
+   console.log("The min temperature is " + data.list[24].main.temp_min);
+   console.log("The max temperature is " + data.list[24].main.temp_max);
+
+   console.log("Friday")
+   console.log("The current temperature is " + data.list[34].main.temp);
+   console.log(`Weather Icon: ${data.list[34].weather[0].description}`);
+   console.log("The min temperature is " + data.list[34].main.temp_min);
+   console.log("The max temperature is " + data.list[34].main.temp_max);
+
+
+});
