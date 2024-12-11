484130c06f307356e9806a2cd893107c3d9ffa3c
xser56 <gettosheep56@gmail.com>
2024-12-10 23:55:05 -0800

    function on temps

---
+++ README.md
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+make array for different days
+figure out local save
+Switch statement for icons
+favorite button
---
+++ scripts/app.js
@@ -0,0 +1,83 @@
+import { myKey } from "./key.js";
+
+// Day Temps
+// const temperatures = 
+// [
+//     { temp: document.getElementById("currentDayTemp") },
+//     { temp: document.getElementById("day1") },
+//     { temp: document.getElementById("day2") },
+//     { temp: document.getElementById("day3") },
+//     { temp: document.getElementById("day4") }
+// ];
+
+let currentTemp = document.getElementById("currentTemp");
+let lowTemp = document.getElementById("lowTemp");
+let highTemp = document.getElementById("highTemp");
+let city = document.getElementById("city");
+
+let weatherIcon = document.getElementById("weatherIcon");
+let weatherIcon2 = document.getElementById("weatherIcon2");
+let weatherIcon3 = document.getElementById("weatherIcon3");
+let weatherIcon4 = document.getElementById("weatherIcon4");
+
+let day1 = document.getElementById("day1");
+let day2 = document.getElementById("day2");
+let day3 = document.getElementById("day3");
+let day4 = document.getElementById("day4");
+
+// Test Button
+let testButton = document.getElementById("testButton");
+
+async function getAPI()
+{
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?=&lat=37.961632&lon=-121.275604&units=imperial&appid=${myKey}`);
+    const data = await response.json();
+    return data;
+}
+
+async function getWeekAPI() 
+{
+    const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?exclude=minutely,alerts,hourly&lat=37.961632&lon=-121.275604&appid=${myKey}&units=imperial`);
+    const data = await response.json();
+    return data;    
+}
+
+
+testButton.addEventListener("click", async function()
+{
+    let data = await getAPI();
+    let weekData = await getWeekAPI();
+    
+    // Check data API
+    console.log("Current Day")
+    console.log("The current temperature is " + Math.trunc(data.list[0].main.temp));
+    console.log(`Weather Icon: ${data.list[0].weather[0].icon}`);
+    console.log("The min temperature is " + data.list[0].main.temp_min);
+    console.log("The max temperature is " + data.list[0].main.temp_max);
+    console.log("city " + data.city.name, data.city.country);
+    console.log("Day 1" + data.list[8].dt_txt);
+
+    // Current Day
+    weatherIcon.innerText = data.list[0].weather[0].icon;
+    currentTemp.innerText = Math.trunc(data.list[0].main.temp);
+    lowTemp.innerText = Math.trunc(data.list[0].main.temp_min);
+    highTemp.innerText = Math.trunc(data.list[0].main.temp_max);
+    city.innerText = data.city.name, data.city.country;
+
+    // 1st Day
+    weatherIcon1.innerText = weekData.daily[0].weather[0].icon;
+    day1.innerText = Math.round(weekData.daily[0].temp.day);
+
+    // 2nd Day
+    weatherIcon2.innerText = weekData.daily[1].weather[0].icon;
+    day2.innerText = Math.round(weekData.daily[1].temp.day);
+
+    // 3rd Day
+    weatherIcon3.innerText = weekData.daily[2].weather[0].icon;
+    day3.innerText = Math.round(weekData.daily[2].temp.day);
+
+    // 4th Day
+    weatherIcon4.innerText = weekData.daily[3].weather[0].icon;
+    day4.innerText = Math.round(weekData.daily[3].temp.day);
+
+});
