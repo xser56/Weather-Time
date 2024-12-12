24506ad71f1a11e0377c614ae350d1821096be7c
xser56 <gettosheep56@gmail.com>
2024-12-11 23:55:40 -0800

    Day of week done

--- README.md
+++ README.md
@@ -7,12 +7,10 @@ Make grid for card placement or something
 Finish styling (font, colors, drop shadow, etc)
 
 Js:
-Need different days of the week (make array or something)
 Search
 Local save
 Hook up icons
 Favorite button
-Finish recommend function - grab random foods from online or ai
 
 Refactor Code:
 make array for variables / elements
--- scripts/app.js
+++ scripts/app.js
@@ -14,9 +14,17 @@ let recKey = document.getElementById("recKey");
 let recValue = document.getElementById("recValue");
 
 let day1 = document.getElementById("day1");
+let dayWeek1 = document.getElementById("dayWeek1");
+
 let day2 = document.getElementById("day2");
+let dayWeek2 = document.getElementById("dayWeek2");
+
 let day3 = document.getElementById("day3");
+let dayWeek3 = document.getElementById("dayWeek3");
+
 let day4 = document.getElementById("day4");
+let dayWeek4 = document.getElementById("dayWeek4");
+
 
 let testButton = document.getElementById("testButton");
 
@@ -90,20 +98,28 @@ testButton.addEventListener("click", async function()
     city.innerText = data.city.name, data.city.country;
 
     // 1st Day
+    let day1Date = new Date(weekData.daily[1].dt * 1000);
     weatherIcon1.innerText = weekData.daily[0].weather[0].icon;
     day1.innerText = Math.round(weekData.daily[0].temp.day);
+    dayWeek1.innerText = daysOfWeek[day1Date.getDay()]; 
 
     // 2nd Day
+    let day2Date = new Date(weekData.daily[2].dt * 1000);
     weatherIcon2.innerText = weekData.daily[1].weather[0].icon;
     day2.innerText = Math.round(weekData.daily[1].temp.day);
+    dayWeek2.innerText = daysOfWeek[day2Date.getDay()];
 
     // 3rd Day
+    let day3Date = new Date(weekData.daily[3].dt * 1000);
     weatherIcon3.innerText = weekData.daily[2].weather[0].icon;
     day3.innerText = Math.round(weekData.daily[2].temp.day);
+    dayWeek3.innerText = daysOfWeek[day3Date.getDay()];
 
     // 4th Day
+    let day4Date = new Date(weekData.daily[4].dt * 1000);
     weatherIcon4.innerText = weekData.daily[3].weather[0].icon;
     day4.innerText = Math.round(weekData.daily[3].temp.day);
+    dayWeek4.innerText = daysOfWeek[day4Date.getDay()];
 
     let recommendTab = await grabRandomFood();
     let foodKey = Object.keys(recommendTab);
